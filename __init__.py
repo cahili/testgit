5970d42c4ae44f794e42535c447e41e6f7920cf9
huerke <huerke@yeah.net>
2016-09-07 08:53:40 +0800

    test

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: huerke
+# @Date:   2016-08-31 09:45:09
+# @Last Modified by:   huerke
+# @Last Modified time: 2016-08-31 15:53:10
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+db = SQLAlchemy()
+bootstrap = Bootstrap()
---
+++ modols.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @Author: huerke
+# @Date:   2016-08-31 09:47:25
+# @Last Modified by:   huerke
+# @Last Modified time: 2016-08-31 19:45:36
+from . import db
+from datetime import datetime
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
